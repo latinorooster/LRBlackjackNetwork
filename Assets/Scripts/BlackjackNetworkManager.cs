f866c5fbb95b19789522b85a9b818344c358c63c
latinorooster <latino_rooster@hotmail.com>
2018-01-05 15:30:50 +1100

    Blackjack multiplayer working

---
+++ Assets/Scripts/BlackjackNetworkManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Rendering;
+
+public class BlackjackNetworkManager : NetworkManager {
+
+    void Awake()
+        {
+            if (isHeadless())
+            {
+                StartServer();
+            }
+        }
+
+    bool isHeadless()
+    {
+        return SystemInfo.graphicsDeviceType == GraphicsDeviceType.Null;
+    }
+}
